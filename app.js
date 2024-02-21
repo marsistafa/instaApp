5d7851386508e42f72f296b9f92a38a66dac3e14
marsi <marsionastafa17@gmail.com>
2024-02-21 15:31:23 +0100

    fixes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 app.get('/', (req, res) => {
-    res.send('Hello Worlddd!')
+    res.send('Hello!')
   })
 
 // Endpoint to initiate video reel upload
@@ -18,7 +18,7 @@ app.get('/upload-video-reel', async (req, res) => {
     const videoPath = req.query.videoPath;
     const description = req.query.description;
 
-    const accessToken = 'EAANVvne9rUwBOz13IgUC1P8Tl0YMbS9DDZAtp9g2CjcZCJB72GVOYfUdlcxw5A4kcRuZApZBxS4ar8hC8N4ZApFgueAJvZCmNjWSNIxBFZCl6olkOD9rLR5oIXynoZCj4kZA4AYP44rWcBnykDPfV8s3atk04MuTl6rbBb3QF4m6dCaIGTSK15Rc0Qq9yP1ZCZA3LZCPexmPN835TbRJifyz1emwti4ZD';
+    const accessToken = 'EAANVvne9rUwBOZCDzvXEoP4bFblvWcMVx2q18RvmouLhYE22jKPbymYJTHP0Pvh9zdShVbEpfuiSRSuZCmyIVMYVlBJqgsHVraerkykNAlHc9JQfOZAEZAD3VIhXWTZChpnXTtbeprdzgfEPVKBzumFy6ZBXWjKiZAdTZBAXLHeAXsCx8Ecam74fRCoIDtwsBqXqt3XkMQY5SEc5T76IC8p1AzWL';
     const pageId = '160868800643701'; 
 
     try {
@@ -27,43 +27,49 @@ app.get('/upload-video-reel', async (req, res) => {
             `https://graph.facebook.com/v19.0/${pageId}/video_reels`,
             {
                 upload_phase: 'start',
-                // file_size: fs.statSync(videoPath).size,
                 access_token: accessToken,
             }, {
                 headers: {
                   'Content-Type': 'application/json',
                 },
             });
-            // console.log(startUploadResponse);
+  
 
         
         const { video_id } = startUploadResponse.data;
  
-        // Step 2: Upload the video file in chunks (for simplicity, this assumes the video fits in one chunk)
-        // const videoData = fs.createReadStream(videoPath);
-        // const fileSize = fs.statSync(videoPath).size;
+        // Step 2: Upload the video
             console.log(video_id);
+
+        axios.get(`https://8557-80-90-84-209.ngrok-free.app/IMG_6303_1.mp4`, null, {
+        headers: {
+            'ngrok-skip-browser-warning': true,
+        },
+        })
+        .then(response => {
+            console.log('ok:');
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          });
+
        const response = await axios.post(`https://rupload.facebook.com/video-upload/v19.0/${video_id}`, null, {
-      headers: {
-        'Authorization': 'OAuth EAANVvne9rUwBOz13IgUC1P8Tl0YMbS9DDZAtp9g2CjcZCJB72GVOYfUdlcxw5A4kcRuZApZBxS4ar8hC8N4ZApFgueAJvZCmNjWSNIxBFZCl6olkOD9rLR5oIXynoZCj4kZA4AYP44rWcBnykDPfV8s3atk04MuTl6rbBb3QF4m6dCaIGTSK15Rc0Qq9yP1ZCZA3LZCPexmPN835TbRJifyz1emwti4ZD',
-        'file_url': 'https://upload.wikimedia.org/wikipedia/commons/transcoded/0/09/Frozen_drop.webm/Frozen_drop.webm.720p.vp9.webm',
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    });
+        headers: {
+            'Authorization': 'OAuth EAANVvne9rUwBOZCDzvXEoP4bFblvWcMVx2q18RvmouLhYE22jKPbymYJTHP0Pvh9zdShVbEpfuiSRSuZCmyIVMYVlBJqgsHVraerkykNAlHc9JQfOZAEZAD3VIhXWTZChpnXTtbeprdzgfEPVKBzumFy6ZBXWjKiZAdTZBAXLHeAXsCx8Ecam74fRCoIDtwsBqXqt3XkMQY5SEc5T76IC8p1AzWL',
+            'file_url': 'https://8557-80-90-84-209.ngrok-free.app/IMG_6303_1.mp4',
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        });
 
-    // console.log(response.data);
+    
+        console.log(response.data);
 
-        const formData = new FormData();
-        formData.append('upload_phase', 'finish');
-        // formData.append('video_file_chunk', videoData, { filename: 'funnyreels.mp4' });
-
-        // console.log('formdata:',formData);
+        // Step 3: Publish the reel using the uploaded video
         const publishResponse = await axios.post(
             `https://graph.facebook.com/v19.0/${pageId}/video_reels`,
-            formData,
+            null,
             {
                 headers: {
-                    ...formData.getHeaders(),
                     'Content-Type': 'multipart/form-data', 
                 },
                 params: {
@@ -76,23 +82,6 @@ app.get('/upload-video-reel', async (req, res) => {
             }
         );        
             console.log(publishResponse);
-        // Step 3: Finish the upload session
-        // await axios.post(`https://graph.facebook.com/v19.0/${pageId}/video_reels`, {
-        //     upload_phase: 'FINISH',
-        //     // upload_session_id: upload_session_id,
-        //     access_token: accessToken,
-       
-        // });
-     
-        // Step 4: Publish the reel using the uploaded video
-        // const publishResponse = await axios.post(
-        //     `https://graph.facebook.com/v19.0/${pageId}/video_reels`,
-        //     {
-        //         video_id: video_id,
-        //         description: description,
-        //         access_token: accessToken,
-        //     }
-        // );
 
         res.json({ success: true, video_id: video_id, publishResponse: publishResponse.data });
     } catch (error) {
